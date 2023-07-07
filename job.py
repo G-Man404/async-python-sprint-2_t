37320ce47c171fd17da90a2fdf3bb45bb73eff2d
gman <anisimovf94@gmail.com>
2023-07-07 14:16:40 +0300

    v 0.0

--- job.py
+++ job.py
@@ -1,12 +1,43 @@
+import datetime
+import time
+import os
+from multiprocessing import Process
+
+
 class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
-        pass
+    def __init__(self, task, args, start_at=datetime.datetime.now(), max_working_time=-1, tries=0, dependencies=[]):
+        self.start_at = start_at
+        self.max_working_time = max_working_time
+        self.tries = tries
+        self.dependencies = dependencies
+        self.task = task
+        self.args = args
+        self.status = "W"
+
+    def dependency_completed(self):
+        status = True
+        for task in self.dependencies:
+            if not task.status:
+                status = False
+        return status
 
     def run(self):
-        pass
+        if self.tries >= 0:
+            if not self.dependency_completed():
+                self.tries -= 1
+                raise "dependency_error"
+            self.status = "P"
+            process = Process(target=self.task, args=(self.args, ))
 
-    def pause(self):
-        pass
+            start_time = datetime.datetime.now()
+            process.start()
 
-    def stop(self):
-        pass
+            while process.is_alive():
+                if datetime.datetime.now() > start_time+datetime.timedelta(seconds=self.max_working_time) and\
+                        self.max_working_time > 0:
+                    process.terminate()
+                    raise TimeoutError
+            self.status = "C"
+        else:
+            self.status = "F"
+            raise "amount_attempts"
--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,43 @@
-class Scheduler:
-    def __init__(self, pool_size=10):
-        pass
+import datetime
+import json
+import sys
+import job
 
-    def schedule(self, task):
-        pass
+
+class Scheduler:
+    def __init__(self, pool_size: int = 10) -> None:
+        self.pool_size = pool_size
+        self.tasks = []
+        self.tasks_in_progress = []
 
     def run(self):
-        pass
+        while True:
+            if len(self.tasks) > 0:
+                if len(self.tasks_in_progress) < self.pool_size:
+                    for task in self.tasks:
+                        if task.start_at < datetime.datetime.now():
+                            self.tasks_in_progress.append(task)
+                            self.tasks.remove(task)
+                            self.tasks_in_progress[-1].run()
+            else:
+                print("All tasks are completed")
+                self.stop()
 
     def restart(self):
-        pass
+        with open("data/tasks.json", "r") as file:
+            json_data = json.load(file)
+            self.tasks = json_data["tasks"]
+            self.tasks_in_progress = json_data["tasks_in_progress"]
+        self.run()
 
     def stop(self):
-        pass
+        if len(self.tasks) > 0:
+            with open("data/tasks.json", "w") as file:
+                json.dump({"tasks": self.tasks, "tasks_in_progress": self.tasks_in_progress}, file)
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    scheduler = Scheduler()
+    scheduler.tasks.append(job.Job(print, "123", datetime.datetime.now()))
+    scheduler.run()
