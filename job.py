819fe3446fb03a128c06540aa03cf6c95a955a0f
gman <anisimovf94@gmail.com>
2023-07-08 02:23:07 +0300

    v 0.1

--- job.py
+++ job.py
@@ -1,12 +1,14 @@
+import datetime
+import time
+import os
+from multiprocessing import Process
+import scheduler
+
+
 class Job:
-    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
-        pass
-
     def run(self):
-        pass
-
-    def pause(self):
-        pass
-
-    def stop(self):
-        pass
+        task = yield
+        try:
+            task.result = task.task(task.args)
+        except:
+            task.tries -= 1
--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,24 @@
+import datetime
+import json
+import sys
+import job
+from dataclasses import dataclass
+
+
+@dataclass
+class Task:
+    task: callable
+    args: list = list
+    start_at: datetime.datetime = datetime.datetime.now()
+    max_working_time: int = -1
+    tries: int = 0
+    dependencies: list = list
+    status: str = "Queue"
+    result: list = list
+
+
 class Scheduler:
-    def __init__(self, pool_size=10):
-        pass
+    def __init__(self, tasks: list[Task], pool_size: int = 10):
+        self.tasks = tasks
+        self.pool_size = pool_size
 
-    def schedule(self, task):
-        pass
-
-    def run(self):
-        pass
-
-    def restart(self):
-        pass
-
-    def stop(self):
-        pass
