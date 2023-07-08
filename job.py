1278c7320a53eb8bd8328fc69c49555e93918e1a
gman <anisimovf94@gmail.com>
2023-07-08 03:08:25 +0300

    v 0.2

--- job.py
+++ job.py
@@ -2,13 +2,31 @@ import datetime
 import time
 import os
 from multiprocessing import Process
+from threading import Thread
 import scheduler
 
 
 class Job:
+
+    def __init__(self, task, args, start_at=datetime.datetime.now(), max_working_time=-1, tries=0, dependencies=list):
+        self.task: callable = task
+        self.args: list = args
+        self.start_at: datetime.datetime = start_at
+        self.max_working_time: int = max_working_time
+        self.tries: int = tries
+        self.dependencies: list = dependencies
+        self.status: str = "Queue"
+        self.result: list = list
+
     def run(self):
-        task = yield
-        try:
-            task.result = task.task(task.args)
-        except:
-            task.tries -= 1
+        thread = Thread(target=self.task, args=self.args)
+        start_time = datetime.datetime.now()
+        thread.start()
+        while thread.is_alive():
+            if self.max_working_time == -1 or\
+                    datetime.datetime.now() < start_time + datetime.timedelta(seconds=self.max_working_time):
+                self.tries -= 1
+                break
+            yield
+        self.result = thread
+        self.status = "F"
--- scheduler.py
+++ scheduler.py
@@ -5,18 +5,6 @@ import job
 from dataclasses import dataclass
 
 
-@dataclass
-class Task:
-    task: callable
-    args: list = list
-    start_at: datetime.datetime = datetime.datetime.now()
-    max_working_time: int = -1
-    tries: int = 0
-    dependencies: list = list
-    status: str = "Queue"
-    result: list = list
-
-
 class Scheduler:
     def __init__(self, tasks: list[Task], pool_size: int = 10):
         self.tasks = tasks
